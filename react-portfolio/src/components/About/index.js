fdd1044bfa1617027d8acda875f0dbec1a3836de
Shashank <shashankintouch@gmail.com>
2022-04-01 19:26:33 +0530

    react portfolio

---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react'
+import {
+  faJava,
+  faCss3,
+  faReact,
+  faHtml5,
+  faPython,
+  faGit,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a full-stack developer actively looking for any projects
+            which allow me to learn new technologies and push my problem-solving skills
+          </p>
+          <p align="LEFT">
+            My main advantage is adapability to any new technology, Iam eager to learn and explore my passion for technology
+          </p>
+          <p>
+           Iam actively looking for new technologies and new project to add to my portfolio
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faPython} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGit} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
